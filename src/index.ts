8cae316cc661abef00b64ac0eef6a30ac5549dda
Wesley Deitch <deitchwesley@gmail.com>
2021-01-30 16:46:38 -0800

    Created User class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ name: 'new record', age: 0 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.save();
