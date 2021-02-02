36bd5165541ffb31bbd424ad12120aa968797660
Wesley Deitch <deitchwesley@gmail.com>
2021-02-01 17:43:40 -0800

    Finished view classes for user and collection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import { User } from './models/User';
-import { UserForm } from './views/UserForm';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const user = User.buildUser({ name: 'John', age: 100 });
-const root = document.getElementById('root');
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
-} else {
-  throw new Error('root element not found');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
